a5a49d517b9fd0309e7a6eb2a450610f899d7e67
Татьяна Пипериди <tanya.piperidi@gmail.com>
2024-08-12 21:24:27 +0300

    one commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const buttonAddCard = document.querySelector('.profile__add-button');
+const listCard = document.querySelector('.places__list');
+// добавление карточки
+buttonAddCard.addEventListener('click', function() {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  listCard.append(cardItem);
+  // цикл присваивания картинки и заголовка
+  const cardTitle = listCard.querySelectorAll('.card__title')
+  // const cardTitleArr = Array.from(cardTitle)
+  const cardImage = listCard.querySelectorAll('.card__image')
+  // const cardImageArr = Array.from(cardImage)
+  // cardImageArr.forEach(function(item) {
+  //   console.log('hello')
+  // })
+  for (let i = 0; i < cardImage.length; i++) {
+    cardTitle[i].textContent = initialCards[i].name
+    cardImage[i].setAttribute('src', initialCards[i].link)
+    console.log('hello')
+  }
+  // удаление карточки
+  cardItem.querySelector('.card__delete-button').addEventListener('click', function() {
+    cardItem.remove();
+  })
+})
+
+
+
+
+
