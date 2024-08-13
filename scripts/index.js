556c55260ae88f1ce1a05ef47773b364d5cb34b9
Татьяна Пипериди <tanya.piperidi@gmail.com>
2024-08-14 02:07:28 +0300

    before checking

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,48 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const listCard = document.querySelector('.places__list');
+const buttonAddCard = document.querySelector('.profile__add-button');
+
+// переменная чтобы вывести сразу 6 карточек
+
+const minOutputCardItem = 6;
+
+// счётчик карточек 
+
+let cardIndex = 0;
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function listItemCardAdd(name, link) {
+  if (cardIndex < initialCards.length) {
+    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    listCard.append(cardItem);
+    const cardTitle = cardItem.querySelector('.card__title');
+    const cardImage = cardItem.querySelector('.card__image');
+    cardTitle.textContent = name;
+    cardImage.setAttribute('src', link);
+    cardIndex++;
 
-// @todo: Вывести карточки на страницу
+    // @todo: Функция удаления карточки
 
-const buttonAddCard = document.querySelector('.profile__add-button');
-const listCard = document.querySelector('.places__list');
-// добавление карточки
+    cardItem.querySelector('.card__delete-button').addEventListener('click', function() {
+      cardItem.remove();
+    });
+  };
+};
+
+// цикл чтобы вывести сразу 6 карточек
+
+for (let i = 0; i < minOutputCardItem; i++) {
+listItemCardAdd(initialCards[cardIndex].name, initialCards[cardIndex].link);
+};
+
+// @todo: Вывести карточки на страницу по нажатию кнопки buttonAddCard
+
 buttonAddCard.addEventListener('click', function() {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-  listCard.append(cardItem);
-  // цикл присваивания картинки и заголовка
-  const cardTitle = listCard.querySelectorAll('.card__title')
-  // const cardTitleArr = Array.from(cardTitle)
-  const cardImage = listCard.querySelectorAll('.card__image')
-  // const cardImageArr = Array.from(cardImage)
-  // cardImageArr.forEach(function(item) {
-  //   console.log('hello')
-  // })
-  for (let i = 0; i < cardImage.length; i++) {
-    cardTitle[i].textContent = initialCards[i].name
-    cardImage[i].setAttribute('src', initialCards[i].link)
-    console.log('hello')
-  }
-  // удаление карточки
-  cardItem.querySelector('.card__delete-button').addEventListener('click', function() {
-    cardItem.remove();
-  })
+  listItemCardAdd(initialCards[cardIndex].name, initialCards[cardIndex].link);
 })
-
-
-
-
-
