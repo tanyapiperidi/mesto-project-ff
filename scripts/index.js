694f3751688076ffafd1828d17baf2bffa6cdc8e
Татьяна Пипериди <tanya.piperidi@gmail.com>
2024-08-20 18:23:32 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -14,11 +14,10 @@ let cardIndex = 0;
 // @todo: Функция создания карточки
 
 function listItemCardAdd() {
-  
-    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-    const cardTitle = cardItem.querySelector('.card__title');
-    const cardImage = cardItem.querySelector('.card__image');
-    if (cardIndex < initialCards.length) {
+  const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardTitle = cardItem.querySelector('.card__title');
+  const cardImage = cardItem.querySelector('.card__image');
+  if (cardIndex < initialCards.length) {
     cardTitle.textContent = initialCards[cardIndex].name;
     cardImage.setAttribute('src', initialCards[cardIndex].link);
     cardIndex++;
